83e3fd2bd19aa96b5af4ca2838f0945130952406
isrhael.christian <isrhael.christian@>
2018-04-10 18:29:27 -0300

    Add React

---
+++ src/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var React = require('react')
+
+export default class Title extends React.Component {
+    render() {
+        return React.createElement('h1', null, 'Olá Mundo')
+    }
+}
