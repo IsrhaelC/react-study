63b493465d553c9a312fa3a2a021a472176a6276
isrhael.christian <isrhael.christian@>
2018-04-17 18:52:54 -0300

    Forms radio and checkbox

--- src/app.js
+++ src/app.js
@@ -1,15 +1,41 @@
 'use strict'
 
 import React from 'react'
-import Button from './button'
 
 export default class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      value: 'valor inicial',
+      checked: false
+    }
+  }
+
   render () {
     return (
       <div>
-        <Button>
-          Clique em mim
-        </Button>
+        <form>
+          <input type='text' value={this.state.value} onChange={(e) => {
+            this.setState({
+              value: e.target.value
+            })
+          }} />
+
+          <label>
+            <input type='checkbox' checked={this.state.checked} onChange={(e) => {
+              this.setState({ checked: e.target.checked })
+            }} />
+            Checkbox
+          </label>
+          <label>
+            <input type='radio' name='rd' value='1' />
+            Radio 1
+          </label>
+          <label>
+            <input type='radio' name='rd' value='2' />
+            Radio 2
+          </label>
+        </form>
       </div>
     )
   }
